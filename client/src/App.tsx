e8674c7a5de42b3d0e6bdb77cc2bb81ea183dc71
Sahil Ahmed <sahilahmed466@gmail.com>
2025-07-08 20:08:40 +0530

    Setting up directories

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { ModeToggle } from './components/mode-toggle'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-md'>app home
+        <ModeToggle />
+      </div>
+    </>
+  )
+}
+
+export default App
