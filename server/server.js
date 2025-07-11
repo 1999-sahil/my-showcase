12a357716a297b1d2b7791ea0f779a7742a052ef
Sahil Ahmed <sahilahmed466@gmail.com>
2025-07-11 19:21:40 +0530

    server: connecting database and creating auth API

--- server/server.js
+++ server/server.js
@@ -1,1 +1,32 @@
-console.log("Server");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const authRoute = require("./routes/authRoute");
+
+const app = express();
+
+// Middleware to handle CORS
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoute);
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
+
+// Start the Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
